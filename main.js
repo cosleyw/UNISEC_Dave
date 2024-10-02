ef31faf5012c4b4eb7bc067c4afb1a8cefd2a974
Nicholas Pfalzgraf <153227910+NickPfalzgraf@users.noreply.github.com>
2024-10-02 18:11:45 -0500

    I guess I'll take Andy's advice ;( (#2)
    * Updated... For Andy..
    
    * Updated
    
    ---------
    
    Co-authored-by: Nicholas Pfalzgraf <nickpfalz@gmail.com>
    Co-authored-by: pfalzgrn <pfalzgrn@uni.edu>

--- main.js
+++ main.js
@@ -1,26 +1,39 @@
 const { Client, Events, GatewayIntentBits } = require("discord.js");
 const { token } = require("./config.json");
 
-
 const dave = new Client({ intents: [
-	GatewayIntentBits.Guilds,
-	GatewayIntentBits.GuildMessages,
-	GatewayIntentBits.MessageContent
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent
 ]});
 
+const responses = [
+    "shutup",
+    "peeps are garbage",
+    "the chicago bears suck",
+    "be quiet",
+    "leave....",
+    "Did you sleep in again...?",
+    "You're fired!",
+    "Tenure removed...",
+    "Make Dave Great Again",
+    "Will this guy ever be quiet?"
+];
+
 dave.once(Events.ClientReady, readyClient => {
-	console.log(`Ready! logged in as ${readyClient.user.tag}`);
+    console.log(`Ready! Logged in as ${readyClient.user.tag}`);
 });
 
 dave.on(Events.MessageCreate, message => {
-	console.log(
-		message.author.displayName,
-		message.content,
-	);
+    console.log(
+        message.author.displayName,
+        message.content,
+    );
 
-	if(Math.random() < 0.047){
-		message.channel.send("shutup");
-	}
+    if (Math.random() < 0.05) {
+        const randomResponse = responses[Math.floor(Math.random() * responses.length)];
+        message.channel.send(randomResponse);
+    }
 });
 
 dave.login(token);
